43a80749a28fb168c64f0f4ae42b9a72bcbabd82
Lyarkh <lcemanuel.emanuel@gmail.com>
2022-12-04 13:07:18 -0300

    Migrações
    Adicionando models para o db e criação de testes para add items no db

--- Flask_Course_Web_Aplication/flask_course_web_application/Flask_Market/market.py
+++ Flask_Course_Web_Aplication/flask_course_web_application/Flask_Market/market.py
@@ -1,6 +1,23 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+db = SQLAlchemy(app)
+
+
+class Item(db.Model):
+    __tablename__ = 'item'
+
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(length=30), nullable=False, unique=True)
+    price = db.Column(db.Integer(), nullable=False)
+    barcode = db.Column(db.String(length=12), nullable=False, unique=True)
+    description = db.Column(db.String(length=1024), nullable=False)
+
+    def __repr__(self):
+        return f'<Item {self.name}>'
 
 @app.route('/')
 @app.route('/home')
@@ -9,12 +26,9 @@ def home_page():
 
 @app.route('/market')
 def market_page():
-    items = [
-        {'id': 1, 'name': 'Phone', 'barcode': '456978123645', 'price': 500},
-        {'id': 2, 'name': 'iPhone', 'barcode': '456987654321', 'price': 900},
-        {'id': 3, 'name': 'iPad', 'barcode': '456987659451', 'price': 150}
-    ]
+    items = Item.query.all()
     return render_template('market.html', items=items)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
