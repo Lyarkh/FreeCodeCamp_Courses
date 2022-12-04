7916512564ba3a2a6b28c5fe7cace2de0e63acae
Lyarkh <lcemanuel.emanuel@gmail.com>
2022-12-04 16:52:02 -0300

    Adicionando user no database utilizando o form

--- Flask_Course_Web_Application/flask_course_web_application/Flask_Market/market/routes.py
+++ Flask_Course_Web_Application/flask_course_web_application/Flask_Market/market/routes.py
@@ -1,6 +1,6 @@
-from market import app
-from market.models import Item
-from flask import render_template
+from market import app, db
+from market.models import Item, User
+from flask import render_template, redirect, url_for
 from market.forms import RegisterForm
 
 
@@ -14,7 +14,17 @@ def market_page():
 	items = Item.query.all()
 	return render_template('market.html', items=items)
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register_page():
     form = RegisterForm()
+    if form.validate_on_submit():
+        user_to_create = User(
+			username=form.username.data,
+			email_address=form.email_address.data,
+			password_hash=form.password1.data)
+        db.session.add(user_to_create)
+        db.session.commit()
+        return redirect(url_for('market_page'))
+
+
     return render_template('register.html', form=form)
