56f3357a6f3daefd935aaee5319e3320bbdcc64c
Lyarkh <lcemanuel.emanuel@gmail.com>
2022-12-04 13:53:21 -0300

    Modificações DB
    Criando model 'User' e criando relações user-item

--- Flask_Course_Web_Application/flask_course_web_application/Flask_Market/market/models.py
+++ Flask_Course_Web_Application/flask_course_web_application/Flask_Market/market/models.py
@@ -1,5 +1,15 @@
 from market import db
 
+class User(db.Model):   # type: ignore
+    __tablename__ = 'user'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(length=30), nullable=False, unique=True)
+    email_address = db.Column(db.String(length=50), nullable=False, unique=True)
+    password_hash = db.Column(db.String(length=16), nullable=False)
+    budget = db.Column(db.Integer(), nullable=False, default=1000)
+    items = db.relationship('Item', backref='owned_user', lazy=True)
+
 
 class Item(db.Model):   # type: ignore
     __tablename__ = 'item'
@@ -9,6 +19,8 @@ class Item(db.Model):   # type: ignore
     price = db.Column(db.Integer(), nullable=False)
     barcode = db.Column(db.String(length=12), nullable=False, unique=True)
     description = db.Column(db.String(length=1024), nullable=False)
+    owner = db.Column(db.Integer(), db.ForeignKey('user.id'))
+
 
     def __repr__(self):
         return f'<Item {self.name}>'
