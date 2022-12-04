50ef46e02712ebb5da160dad97ad2972f80a5a77
Lyarkh <lcemanuel.emanuel@gmail.com>
2022-12-04 17:07:15 -0300

    Adicionando validações no form e print de erros no lado do server

--- Flask_Course_Web_Application/flask_course_web_application/Flask_Market/market/routes.py
+++ Flask_Course_Web_Application/flask_course_web_application/Flask_Market/market/routes.py
@@ -25,6 +25,7 @@ def register_page():
         db.session.add(user_to_create)
         db.session.commit()
         return redirect(url_for('market_page'))
-
-
+    if form.errors != {}:
+        for err_msg in form.errors.values():
+            print(f'Ocorreu um erro ao criar o usuário: {err_msg}')
     return render_template('register.html', form=form)
