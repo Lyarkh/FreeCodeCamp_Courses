2438a34ec9e342da7f6854123a160bd878804ed2
Lyarkh <lcemanuel.emanuel@gmail.com>
2022-12-04 11:14:00 -0300

    Modificações market
    Criando iteração com lista exemplo gerada e modificação route market

--- Flask_Course_Web_Aplication/flask_course_web_aplication/Flask_Market/market.py
+++ Flask_Course_Web_Aplication/flask_course_web_aplication/Flask_Market/market.py
@@ -7,6 +7,14 @@ app = Flask(__name__)
 def hello():
         return render_template('home.html')
 
+@app.route('/market')
+def market_page():
+    items = [
+        {'id': 1, 'name': 'Phone', 'barcode': '456978123645', 'price': 500},
+        {'id': 2, 'name': 'iPhone', 'barcode': '456987654321', 'price': 900},
+        {'id': 3, 'name': 'iPad', 'barcode': '456987659451', 'price': 150}
+    ]
+    return render_template('market.html', items=items)
 
 if __name__ == '__main__':
     app.run(debug=True)
