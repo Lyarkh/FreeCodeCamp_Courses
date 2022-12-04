e5e4b814a33a8ea9ae0642ca9f01a2373052b90f
Lyarkh <lcemanuel.emanuel@gmail.com>
2022-12-04 10:38:34 -0300

    Modificando Home Page e criando base do navbar com opções

--- Flask_Course_Web_Aplication/flask_course_web_aplication/Flask_Market/market.py
+++ Flask_Course_Web_Aplication/flask_course_web_aplication/Flask_Market/market.py
@@ -3,8 +3,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/home')
 def hello():
-    return render_template('index.html')
+        return render_template('home.html')
 
 
 if __name__ == '__main__':
