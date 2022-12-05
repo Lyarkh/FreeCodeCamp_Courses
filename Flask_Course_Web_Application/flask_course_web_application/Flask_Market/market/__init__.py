78a96df09792d8b4c6e28fe3c04e964a23a76fd8
Lyarkh <lcemanuel.emanuel@gmail.com>
2022-12-04 22:25:35 -0300

    ignore typing

--- Flask_Course_Web_Application/flask_course_web_application/Flask_Market/market/__init__.py
+++ Flask_Course_Web_Application/flask_course_web_application/Flask_Market/market/__init__.py
@@ -9,7 +9,7 @@ app.config['SECRET_KEY'] = 'fb80ddf6b95bce3c1893a2df'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
-login_manager.login_view = 'login_page'
+login_manager.login_view = 'login_page'     # type:ignore
 login_manager.login_message_category = 'info'
 
 from market import routes
