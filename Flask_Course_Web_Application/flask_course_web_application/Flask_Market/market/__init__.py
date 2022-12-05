22d192d6e1a1b6354b2b705fdcf65ff44c7342ae
Lyarkh <lcemanuel.emanuel@gmail.com>
2022-12-04 22:25:00 -0300

    Criando login view e login_required em routes que são necessários estarem autenticados

--- Flask_Course_Web_Application/flask_course_web_application/Flask_Market/market/__init__.py
+++ Flask_Course_Web_Application/flask_course_web_application/Flask_Market/market/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
@@ -9,5 +9,7 @@ app.config['SECRET_KEY'] = 'fb80ddf6b95bce3c1893a2df'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
+login_manager.login_view = 'login_page'
+login_manager.login_message_category = 'info'
 
 from market import routes
--- Flask_Course_Web_Application/flask_course_web_application/Flask_Market/market/routes.py
+++ Flask_Course_Web_Application/flask_course_web_application/Flask_Market/market/routes.py
@@ -1,8 +1,8 @@
 from market import app, db
 from market.models import Item, User
-from flask import render_template, redirect, url_for, flash, request
+from flask import render_template, redirect, url_for, flash
 from market.forms import RegisterForm, LoginForm
-from flask_login import login_user, logout_user
+from flask_login import login_user, logout_user, login_required
 
 
 @app.route('/')
@@ -11,25 +11,29 @@ def home_page():
 	return render_template('home.html')
 
 @app.route('/market')
+@login_required
 def market_page():
 	items = Item.query.all()
 	return render_template('market.html', items=items)
 
 @app.route('/register', methods=['GET', 'POST'])
 def register_page():
-    form = RegisterForm()
-    if form.validate_on_submit():
-        user_to_create = User(
-			username=form.username.data,
-			email_address=form.email_address.data,
-			password=form.password1.data)
-        db.session.add(user_to_create)
-        db.session.commit()
-        return redirect(url_for('market_page'))
-    if form.errors != {}:
-        for err_msg in form.errors.values():
-            flash(f'Ocorreu um erro ao criar o usuário: {err_msg[0]}', category='danger')
-    return render_template('register.html', form=form)
+	form = RegisterForm()
+	if form.validate_on_submit():
+		user_to_create = User(
+		username=form.username.data,
+		email_address=form.email_address.data,
+		password=form.password1.data
+		)
+		db.session.add(user_to_create)
+		db.session.commit()
+		login_user(user_to_create)
+		flash(f'Account create successfully! You are now logged in as {user_to_create.username}', category='success')
+		return redirect(url_for('market_page'))
+	if form.errors != {}:
+		for err_msg in form.errors.values():
+			flash(f'Ocorreu um erro ao criar o usuário: {err_msg[0]}', category='danger')
+	return render_template('register.html', form=form)
 
 
 @app.route('/login', methods=['GET', 'POST'])
