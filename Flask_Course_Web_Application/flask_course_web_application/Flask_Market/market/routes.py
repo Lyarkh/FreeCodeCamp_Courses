352bce3106864fd14caff1c4b784d1969e6ebc5e
Lyarkh <lcemanuel.emanuel@gmail.com>
2022-12-04 22:11:25 -0300

    Criando route de logout do usuario

--- Flask_Course_Web_Application/flask_course_web_application/Flask_Market/market/routes.py
+++ Flask_Course_Web_Application/flask_course_web_application/Flask_Market/market/routes.py
@@ -2,7 +2,7 @@ from market import app, db
 from market.models import Item, User
 from flask import render_template, redirect, url_for, flash, request
 from market.forms import RegisterForm, LoginForm
-from flask_login import login_user
+from flask_login import login_user, logout_user
 
 
 @app.route('/')
@@ -44,3 +44,9 @@ def login_page():
 		else:
 			flash('Username and password are incorrect! Please try again.', category='danger')
 	return render_template('login.html', form=form)
+
+@app.route('/logout')
+def logout_page():
+	logout_user()
+	flash('You have been logged out!', category='info')
+	return redirect(url_for('home_page'))
