870f839a8b7c0db081f55f5eae53130a81ef3274
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-01-31 23:03:20 -0300

    feat: adicionando validações na venda do item

--- Flask_Course_Web_Application/flask_course_web_application/Flask_Market/market/routes.py
+++ Flask_Course_Web_Application/flask_course_web_application/Flask_Market/market/routes.py
@@ -32,6 +32,17 @@ def market_page():
 		# Sell Item Logic
 		sold_item = request.form.get('sold_item')
 		s_item_object = Item.query.filter_by(name=sold_item).first()
+		if s_item_object:
+			if current_user.can_sell(s_item_object):
+				s_item_object.sell(current_user)
+				flash(
+					f"Congratulations! You sold {s_item_object.name}"
+					f"back to market!", category='success')
+			else:
+				flash(
+					f"Something went wrong with selling "
+					f"{s_item_object.name}", category='danger')
+
 		return redirect(url_for('market_page'))
 	if request.method == 'GET':
 		items = Item.query.filter_by(owner=None)
