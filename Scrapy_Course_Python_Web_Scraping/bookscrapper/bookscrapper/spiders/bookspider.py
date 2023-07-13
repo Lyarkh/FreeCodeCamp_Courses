ce410d67faf7ed14f7e12a3e3ecdbd21b6663c74
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-07-13 20:21:24 -0300

    feat: fazendo a busca dos livros da página

--- Scrapy_Course_Python_Web_Scraping/bookscrapper/bookscrapper/spiders/bookspider.py
+++ Scrapy_Course_Python_Web_Scraping/bookscrapper/bookscrapper/spiders/bookspider.py
@@ -7,4 +7,11 @@ class BookspiderSpider(scrapy.Spider):
     start_urls = ["https://books.toscrape.com"]
 
     def parse(self, response):
-        pass
+        books = response.css('article.product_pod')
+
+        for book in books:
+            yield {
+                'name': book.css('h3 a::text').get(),
+                'price': book.css('.product_price .price_color::text').get(),
+                'url': book.css('h3 a').attrib['href']
+            }
