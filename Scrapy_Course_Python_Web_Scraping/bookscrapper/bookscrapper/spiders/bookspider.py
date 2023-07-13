0d39c354b52ed602c0ed610f36a8bd456f627152
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-07-13 20:34:04 -0300

    fix/feat: corrigindo bug de busca das paginas, que havia urls diferentes

--- Scrapy_Course_Python_Web_Scraping/bookscrapper/bookscrapper/spiders/bookspider.py
+++ Scrapy_Course_Python_Web_Scraping/bookscrapper/bookscrapper/spiders/bookspider.py
@@ -16,9 +16,11 @@ class BookspiderSpider(scrapy.Spider):
                 'url': book.css('h3 a').attrib['href']
             }
 
-
         next_page = response.css('li.next a :: attr(href)').get()
 
         if next_page is not None:
-            next_page_url = 'https://books.toscrape.com/' + next_page
+            if 'catalogue/' in next_page:
+                next_page_url = 'https://books.toscrape.com/' + next_page
+            else:
+                next_page_url = 'https://books.toscrape.com/catalogue/' + next_page
             yield response.follow(next_page_url, callback=self.parse)
