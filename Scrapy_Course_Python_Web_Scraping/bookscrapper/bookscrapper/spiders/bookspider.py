b88880fbb92c924a76ee73f790327011cc974bc0
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-07-13 20:28:03 -0300

    feat: fazendo a busca do url da proxima pagina

--- Scrapy_Course_Python_Web_Scraping/bookscrapper/bookscrapper/spiders/bookspider.py
+++ Scrapy_Course_Python_Web_Scraping/bookscrapper/bookscrapper/spiders/bookspider.py
@@ -15,3 +15,10 @@ class BookspiderSpider(scrapy.Spider):
                 'price': book.css('.product_price .price_color::text').get(),
                 'url': book.css('h3 a').attrib['href']
             }
+
+
+        next_page = response.css('li.next a :: attr(href)').get()
+
+        if next_page is not None:
+            next_page_url = 'https://books.toscrape.com/' + next_page
+            yield response.follow(next_page_url, callback=self.parse)
