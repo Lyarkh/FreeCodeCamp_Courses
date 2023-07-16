4638669bafe0b4ed5b4621918f9d05c4f939d981
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-07-16 19:02:53 -0300

    feat: criando tratativa na pipeline para tratatina dos espaços em brancos do campo description

--- Scrapy_Course_Python_Web_Scraping/bookscrapper/bookscrapper/pipelines.py
+++ Scrapy_Course_Python_Web_Scraping/bookscrapper/bookscrapper/pipelines.py
@@ -10,4 +10,14 @@ from itemadapter import ItemAdapter
 
 class BookscrapperPipeline:
     def process_item(self, item, spider):
+
+        adapter = ItemAdapter(item)
+        field_names = adapter.field_names()
+        for field_name in field_names:
+            if field_name != 'descriptions':
+                value = adapter.get(field_name)
+                adapter[field_name] = value.strip()
+
+
+                
         return item
