8821df12387731d670aa4828bc24e7e66cfd9720
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-07-17 21:09:22 -0300

    fix: corrigindo dados do spider para a o parser do item

--- Scrapy_Course_Python_Web_Scraping/bookscrapper/bookscrapper/spiders/bookspider.py
+++ Scrapy_Course_Python_Web_Scraping/bookscrapper/bookscrapper/spiders/bookspider.py
@@ -32,19 +32,20 @@ class BookspiderSpider(scrapy.Spider):
         table_rows = response.css('table tr')
         book_item = BookItem()
 
-        book_item['url'] = response.url
-        book_item['title'] = response.css('.product_main h1::text').get()
-        book_item['upc'] = table_rows[0].css('td ::text').get()
-        book_item['product_type'] = table_rows[1].css('td ::text').get()
-        book_item['price_excl_tax'] = table_rows[2].css('td ::text').get()
-        book_item['price_incl_tax'] = table_rows[3].css('td ::text').get()
-        book_item['tax'] = table_rows[4].css('td ::text').get()
-        book_item['availability'] = table_rows[5].css('td ::text').get()
-        book_item['num_reviews'] = table_rows[6].css('td ::text').get()
-        book_item['stars'] = response.css('p.star-rating').attrib['class']
-        book_item['category'] = response.xpath("//ul[@class='breadcrumb']/li[@class='active']/preceding-sibling::li[1]/a/text()").get()
-        book_item['description'] = response.xpath("//div[@id='product_description']/following-sibling::p/text()").get()
-        book_item['price'] = response.css('p.price_color ::text').get()
+        book_item['url'] = response.url,
+        book_item['title'] = response.css('.product_main h1::text').get(),
+        book_item['upc'] = table_rows[0].css("td ::text").get()
+        book_item['product_type' ] = table_rows[1].css("td ::text").get(),
+        book_item['price_excl_tax'] = table_rows[2].css("td ::text").get(),
+        book_item['price_incl_tax'] = table_rows[3].css("td ::text").get(),
+        book_item['tax'] = table_rows[4].css("td ::text").get(),
+        book_item['availability'] = table_rows[5].css("td ::text").get(),
+        book_item['num_reviews']=  table_rows[6].css("td ::text").get(),
+        book_item['stars'] = response.css("p.star-rating").attrib['class'],
+        book_item['category'] = response.xpath("//ul[@class='breadcrumb']/li[@class='active']/preceding-sibling::li[1]/a/text()").get(),
+        book_item['description'] = response.xpath("//div[@id='product_description']/following-sibling::p/text()").get(),
+        book_item['price'] = response.css('p.price_color ::text').get(),
+    
 
 
         yield book_item
