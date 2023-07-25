bb34b11c095ba4c282d39a976c3bb09b8b3b325f
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-07-25 20:56:59 -0300

    feat: habilindando moddlewarer da busca de um fake user-agent

--- Scrapy_Course_Python_Web_Scraping/bookscrapper/bookscrapper/settings.py
+++ Scrapy_Course_Python_Web_Scraping/bookscrapper/bookscrapper/settings.py
@@ -58,9 +58,10 @@ ROBOTSTXT_OBEY = True
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    "bookscrapper.middlewares.BookscrapperDownloaderMiddleware": 543,
-#}
+DOWNLOADER_MIDDLEWARES = {
+    # "bookscrapper.middlewares.BookscrapperDownloaderMiddleware": 543,
+    "bookscrapper.middlewares.ScrapeOpsFakeUserAgentMiddleware": 400,
+}
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
