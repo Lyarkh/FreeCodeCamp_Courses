e2b36673c3756c1972dc2d393e58be49895942d1
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-07-25 20:48:54 -0300

    feat: iniciado classe responável por tratar os fake User-Agents

--- Scrapy_Course_Python_Web_Scraping/bookscrapper/bookscrapper/middlewares.py
+++ Scrapy_Course_Python_Web_Scraping/bookscrapper/bookscrapper/middlewares.py
@@ -109,4 +109,16 @@ import requests
 
 
 class ScrapeOpsFakeUserAgentMiddleware:
-    ...
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(crawler.settings)
+
+    def __init__(self, settings):
+        self.scrapeops_api_key = settings.get('SCRAPEOPS_API_KEY')
+        self.scrapeops_endpoint = settings.get('SCRAPEOPS_FAKE_USER_AGENT_ENDPOINT', 'http://headers.scrapeops.io/v1/user-agents?')
+        self.scrapeops_fake_user_agents_active = settings.get('SCRAPEOPS_FAKE_USER_AGENT_ENABLED', False)
+        self.scrapeops_num_results = settings.get('SCRAPEOPS_NUM_RESULTS')
+        self.headers_list = []
+        self._get_user_agents_list()
+        self._scrapeops_fake_user_agents_enabled()
+
